945491480572bde573099e7a298b459366162c2e
Andreas Meyer Engel <andreasmeyerengel@hotmail.com>
2024-02-14 21:47:07 -0300

    Implementando código para a massa nunca quebrar

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,24 +1,34 @@
 /// <reference types="cypress"  />
 
-import loginPage from '../support/pages/login'
-import dashPage from '../support/pages/dash' 
+import loginPage from "../support/pages/login";
+import dashPage from "../support/pages/dash";
 
-describe('login', function(){
+describe("login", function () {
+  context("quando o usuário tiver as credencias corretas", function () {
+    const user = {
+      name: "Jassa Samurai",
+      email: "jassa@samuraibs.com",
+      password: "pwd123",
+      is_provider: true,
+    };
 
-    context('quando o usuário tiver as credencias corretas', function(){
-        const user = {
-            name: 'Jassa Samurai',
-            email: "jassa@samuraibs.com",
-            password: "pwd123",
-            is_provider: true
-        }
+    before(function () {
+      cy.task("removeUser", user.email).then((result) => {
+        cy.log("Resultado da remoção do usuário:", result);
+      });
+      cy.request("POST", "http://localhost:3333/users", user).then(function (
+        response
+      ) {
+        expect(response.status).to.eq(200);
+      });
+    });
 
-        it('deve logar com sucesso', function(){
-            loginPage.go()
-            loginPage.form(user)
-            loginPage.submit()
+    it("deve logar com sucesso", function () {
+      loginPage.go();
+      loginPage.form(user);
+      loginPage.submit();
 
-            dashPage.header.userLoggedIn(user.name)
-        })
-    })
-})
+      dashPage.header.userLoggedIn(user.name);
+    });
+  });
+});
