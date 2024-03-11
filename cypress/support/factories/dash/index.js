ec9eb48bc239206bc895d49fe927fecdc52c936b
Andreas Meyer Engel <andreasmeyerengel@hotmail.com>
2024-03-11 09:23:14 -0300

    Adicionando factories para o arquivo dash

---
+++ cypress/support/factories/dash/index.js
@@ -0,0 +1,18 @@
+
+exports.customer = {
+    name: 'Nickk Sixx',
+    email: 'sixx@motleycrue.com',
+    password: 'pwd123',
+    is_provider: false
+}
+
+exports.provider = {
+    name: 'Ramon Valdez',
+    email: 'seumadruga@televisa.com',
+    password: 'pwd123',
+    is_provider: true
+}
+
+exports.appointment = {
+    hour: '14:00'
+}
